bd50e409a59bc5fdcdc409d81ddf82bba5aa22b2
Kiet Vo <kietvnz@gmail.com>
2020-01-15 02:21:06 +1300

    Added unit tests and implemation for parsing feed data from the xml file

---
+++ BetEasy.Services/XmlFileParser.cs
@@ -0,0 +1,46 @@
+﻿using BetEasy.Contracts.Configurations;
+using BetEasy.Contracts.Interfaces.Services;
+using BetEasy.Contracts.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BetEasy.Services
+{
+    public class XmlFileParser : IXmlFileParser
+    {
+        private readonly IOptions<AppConfig> _appConfig;
+        private readonly ILogger<XmlFileParser> _logger;
+
+        public XmlFileParser(IOptions<AppConfig> appConfig, ILogger<XmlFileParser> logger)
+        {
+            _appConfig = appConfig;
+            _logger = logger;
+        }
+        public async Task<IEnumerable<Horse>> ParseAsync()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _appConfig.Value.XmlFilePath);
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string text = await sr.ReadToEndAsync();
+
+                XDocument xmlDoc = XDocument.Parse(text);
+                var prices = xmlDoc.Descendants("race").Elements("prices").Single().Descendants("horse");
+                var horses = xmlDoc.Descendants("race").Elements("horses").Single().Descendants("horse");
+
+                return horses.Select(h =>
+                        new Horse
+                        {
+                            Name = h.Attribute("name").Value,
+                            Price = decimal.Parse(prices.Single(p => p.Attribute("number").Value == h.Element("number").Value).Attribute("Price").Value)
+                        }
+                    );
+            }
+        }
+    }
+}
---
+++ BetEasy.Tests/Services/XmlFileParserTests.cs
@@ -0,0 +1,56 @@
+﻿using BetEasy.Contracts.Configurations;
+using BetEasy.Contracts.Models;
+using BetEasy.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BetEasy.Tests.Services
+{
+    public class XmlFileParserTests
+    {
+        [Fact]
+        public async Task ShouldParseJsonFileSuccessfully()
+        {
+            //Arrange
+            var appConfig = new AppConfig { JsonFilePath = "FeedData\\Wolferhampton_Race1.json", XmlFilePath = "FeedData\\Caulfield_Race1.xml" };
+            var IOptionsMock = Options.Create(appConfig);
+
+            var loggerMock = new Mock<ILogger<XmlFileParser>>();
+
+            var xmlFileParser = new XmlFileParser(IOptionsMock, loggerMock.Object);
+
+            //Act 
+            var horses = await xmlFileParser.ParseAsync();
+            Horse horse1 = horses.First();
+            Horse horse2 = horses.Skip(1).First();
+
+            //Assert
+            Assert.True(horses.Count() == 2);
+            Assert.True(horse1.Name == "Advancing"
+                                      && horse1.Price == 4.2m);
+
+            Assert.True(horse2.Name == "Coronel"
+                                       && horse2.Price == 12m);
+        }
+
+        [Fact]
+        public async Task ShouldParseXmlFileWithAnException()
+        {
+            //Arrange
+            var appConfig = new AppConfig { JsonFilePath = "", XmlFilePath = "" };
+            var IOptionsMock = Options.Create(appConfig);
+
+            var loggerMock = new Mock<ILogger<XmlFileParser>>();
+
+            var xmlFileParser = new XmlFileParser(IOptionsMock, loggerMock.Object);
+
+            //Act and Assert
+            await Assert.ThrowsAsync<DirectoryNotFoundException>(() => xmlFileParser.ParseAsync());
+        }
+    }
+}
